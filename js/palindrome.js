fb39705862037342923588e23deb68e15183c7ea
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-11 22:06:41 +0200

    ADD: user choice between Odd or Even; chose a number from 1 to 5; create function give random number from 1 to 5

---
+++ js/palindrome.js
@@ -0,0 +1,9 @@
+let userChoice = prompt('Scegli tra "pari" o "dispari"'); //string | null
+let userNumber = parseInt(prompt('Scegli un numero da 1 a 5')) //number | NaN
+
+
+function randomNumber1_5() { // numero casuale intervallo 1-5
+  return Math.round(Math.random() * 5);
+}
+
+
