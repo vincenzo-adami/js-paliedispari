53c88d601df7ca86f38550606a73f4c87d713e6e
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-11 23:00:13 +0200

    EDIT: chage index second word in if's funciton; edit TYPO line 5; nomalized prompt string to lower case

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,11 +1,12 @@
-let userWord = prompt('Scrivi una parola');
+let userWord = prompt('Scrivi una parola')
+let userWordNeutralized = userWord.toLowerCase();
 
 function isPalindrome(word) { // verifica se la parola è palindroma ||| ritorna un valore boolean
-  let wordLength = word.lenght;
+  let wordLength = word.length;
   let result;
 
   for (let i = 0; i < wordLength; i++) {
-    if (word[i] === word[wordLength - 1 + i]) {
+    if (word[i] === word[(wordLength - 1) - i]) {
       result = true
     } else {
       result = false
@@ -14,10 +15,10 @@ function isPalindrome(word) { // verifica se la parola è palindroma ||| ritorna
   return result
 }
 
-let verifyPalindrome = isPalindrome(userWord)
+let verifyPalindrome = isPalindrome(userWordNeutralized)
 
 if (verifyPalindrome) {
-  console.log('La parola ' + userWord + ' è palindroma')
+  console.log('La parola "' + userWord + '" è palindroma')
 } else {
-  console.log('La parola ' + userWord + ' non è palindroma')
+  console.log('La parola "' + userWord + '" non è palindroma')
 }
